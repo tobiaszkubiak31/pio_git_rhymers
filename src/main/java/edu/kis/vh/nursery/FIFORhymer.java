9c0bc754498255be6e13eca47480cb424252ab34
TKub112 <tobiaszkubiak31@gmail.com>
2020-01-01 23:41:26 +0100

    Formantting fix

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,25 +2,26 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
 
-	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-	/*
-	count out the first element from from stack
-	 */
-	@Override
-	public int countOut() {
+    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+
+    /*
+    count out the first element from from stack
+     */
+    @Override
+    public int countOut() {
 		/*
 		if heap isnt empty, reverse heap
 		 */
-		while (!callCheck())
-			temp.countIn(super.countOut());
-		
-		int ret = temp.countOut();
+        while (!callCheck())
+            temp.countIn(super.countOut());
+
+        int ret = temp.countOut();
 		/*
 		if temp heap isnt empty, reverse heap
 		 */
-		while (!temp.callCheck())
-			countIn(temp.countOut());
-		
-		return ret;
-	}
+        while (!temp.callCheck())
+            countIn(temp.countOut());
+
+        return ret;
+    }
 }
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,22 +2,23 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
-	/*
-	  odrzuc gdy:
-	 1. w kolejce sie cos wgl znajuje  &&
-	 2. liczba na wejsciu jest wieksza od bierzacej
-	 */
-	public void countIn(int in) {
-	//meine
-		System.out.println("last element : " + peekaboo() + " input: " + in);
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
+
+    /*
+      odrzuc gdy:
+     1. w kolejce sie cos wgl znajuje  &&
+     2. liczba na wejsciu jest wieksza od bierzacej
+     */
+    public void countIn(int in) {
+        //meine
+        System.out.println("last element : " + peekaboo() + " input: " + in);
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
