74cef0208865e34480dedc342a2a095b6963d0ec
Tobiasz <tobiaszkubiak31@gmail.com>
2020-01-31 00:53:27 +0100

    1.5 add documentation to DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,30 +1,40 @@
 package edu.kis.vh.nursery;
+/**
+*This class is implementation of DeafultCounringRhymer
+*Gives mechanism of stack data structure
 
+*/
 public class DefaultCountingOutRhymer {
 
     private int[] NUMBERS = new int[12];
 
 
     private int sizePointer = -1;
-
+    /**
+    *Add new element at the end
+     */
     public void pushElement(int in) {
         if (!isFull())
             NUMBERS[++sizePointer] = in;
     }
 
-    /*
-    true if data structure is empty
+    /**
+    * @return true if the stack is empty
      */
     public boolean isEmpty() {
         return sizePointer == -1;
     }
 
+    /**
+     *
+     * @return true if stack is full
+     */
     public boolean isFull() {
         return sizePointer == 11;
     }
 
-    /*
-    return last element of data struccture
+    /**
+    *@return the last element of stack
      */
     protected int getLastElement() {
         if (isEmpty())
@@ -33,6 +43,9 @@ public class DefaultCountingOutRhymer {
         return NUMBERS[sizePointer];
     }
 
+    /**
+     * @return popped elment from stack if stack is empty return -1
+     */
     public int popElement() {
         if (isEmpty())
             return -1;
